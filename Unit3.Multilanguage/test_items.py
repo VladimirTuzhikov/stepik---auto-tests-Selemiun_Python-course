6696dfc01aa42e3ed26a3d33b611d907f1767086
Vladimir Tuzhikov <touzhikov_v@rambler.ru>
2019-12-12 15:38:36 +0100

    procedure name changed to lowercase language variable removed sleep increased to 15 minutes

--- Unit3.Multilanguage/test_items.py
+++ Unit3.Multilanguage/test_items.py
@@ -1,14 +1,13 @@
 import time
 
 
-def test_Multilanguage_Interface(browser, langopt):
-    language = langopt
+def test_multilanguage_interface(browser, langopt):
     # Открываем страницу
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    link = f"http://selenium1py.pythonanywhere.com/{langopt}/catalogue/coders-at-work_207/"
     browser.get(link)
 
     # Ожидание чтобы визуально оценить правильный язык на кпоке "Добавить в крозину"
-    time.sleep(5)
+    time.sleep(15)
 
     # Проверям, что кнопка "Добвать в корзину" находится на странице
     assert browser.find_element_by_css_selector("#add_to_basket_form button"), "Add to bucket button must be located"
